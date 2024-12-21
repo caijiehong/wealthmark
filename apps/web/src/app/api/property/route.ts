3536a5d6d8b9d51942368c05c441a9ce2b7c0660
jonescai <jonescai@tencent.com>
2024-12-21 14:05:11 +0800

    feat: 部署

--- apps/web/src/app/api/property/route.ts
+++ apps/web/src/app/api/property/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse, type NextRequest } from "next/server";
 // import { loadStokInfoUs } from "../../../business/aktools";
 // import dayjs from "dayjs";
-import { db } from "@/app/lib/db";
+import { getModelProperty } from "@/app/lib/db";
 
-export async function POST(req: NextRequest) {
-  const model = await db.getModelProperty();
+export async function GET(req: NextRequest) {
+  const model = await getModelProperty();
 
   const res = await model.findOne({
     where: {
