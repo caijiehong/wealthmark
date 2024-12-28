803019ef90954baab2c0708a3c17ca9de511593c
jonescai <jonescai@tencent.com>
2024-12-28 11:07:28 +0800

    feat: tab 栏支持页面跳转

--- apps/web/src/app/components/tabs/index.tsx
+++ apps/web/src/app/components/tabs/index.tsx
@@ -9,11 +9,22 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import styles from "./index.module.css";
 
+type TabKey = "home" | "chart";
 const Tabs: React.FC<{
-  activeKey: "home" | "chart";
+  activeKey: TabKey;
   children: React.ReactNode;
 }> = ({ activeKey, children }) => {
   const router = useRouter();
+  const onClick = (key: TabKey) => {
+    if (key === activeKey) {
+      return;
+    }
+    if (key === "home") {
+      router.push(`/pages/home`);
+    } else if (key === "chart") {
+      router.push(`/pages/chart`);
+    }
+  };
   return (
     <div className={styles.app}>
       <div className={styles.body}>{children}</div>
@@ -32,8 +43,16 @@ const Tabs: React.FC<{
           <AddOutline fontSize={32} />
         </FloatingBubble>
         <TabBar activeKey={activeKey} safeArea={true}>
-          <TabBar.Item key="home" icon={<UnorderedListOutline />} />
-          <TabBar.Item key="chart" icon={<PieOutline />} />
+          <TabBar.Item
+            key="home"
+            icon={<UnorderedListOutline />}
+            onClick={() => onClick("home")}
+          />
+          <TabBar.Item
+            key="chart"
+            icon={<PieOutline />}
+            onClick={() => onClick("chart")}
+          />
         </TabBar>
       </div>
     </div>
